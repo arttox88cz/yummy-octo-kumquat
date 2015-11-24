21cfb7fbabbd584b70215033d39e421c63e78c99
arttox88cz <i1326689@trbvm.com>
2015-11-24 20:16:32 +0100

    first complete

--- main.sh
+++ main.sh
@@ -18,7 +18,7 @@ set -e
 ##########################
 # copy folders for later #
 ##########################
-cp -R {config,apt,docs} /tmp
+cp -R {config,apt} /tmp
 
 ##############
 # Ask Script #
@@ -39,7 +39,7 @@ fi
 # Last echo #
 ############
 clear
-echo -e "Started! If you wish to quit, perform: su -c killall newbie.sh in a separate terminal."
+echo -e "Started! If you wish to quit, perform: su -c killall main.sh in a separate terminal."
 
 #################################################################
 # Update, make sure sudo is installed, make sure user is sudoer #
@@ -54,31 +54,16 @@ su --preserve-environment -c "apt update && apt install sudo && echo -e '$USER A
 # Prerequisites #						### JUST CHANGE SOURCES.LIST, CHECK ON DRIVERS  ### CAN CHOOSE STABLE/TESTING/UNSTABLE??
 #################
 echo -e "\nPreparing the system..."
-echo -e "e\nWould you like to install these NON-FREE Drivers? [N/y]"
-read -rn1 ans
+echo -e "\nCopying new sources.list..."
 
-if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
-    sudo su -c "echo 'deb http://www.deb-multimedia.org testing main non-free' > /etc/apt/sources.list"
-    sudo cp Apt/{testing,unstable,experimental}-nonfree.list /etc/apt/sources.list.d
+    sudo cp apt/sources.list /etc/apt/sources.list
+    echo -e "\nCopied!"
     sleep 1
     sudo apt update
-    sudo apt install -y firmware-linux-nonfree # My fucking god READ COMMENTS BEFORE CHANGING LoC
+    sudo apt install -y firmware-linux-nonfree
     clear
-    echo -e "The Non-free Repos will be used.You should consider getting a new computer if you require these packages." # stop using random -e's
-else
-    sudo su -c "echo 'deb http://www.deb-multimedia.org testing main' > /etc/apt/sources.list"
-    sudo cp Apt/{testing,unstable,experimental,security}.list /etc/apt/sources.list.d
-    clear
-    echo -e "The FREE Repos will be used. This might mean that some parts of your system (for instance your actual wifi card) are broken due to missing drivers. In case this occurs please use the non-free version instead."
+    echo -e "\nInstalled nonfree drivers!"
     sleep 1
-    sudo apt update
-fi
-
-#############################################################################################
-# The point security contains non-free and contrib packages is moot as it will only provide #
-# Security updates for the pre-existing software on the system                              #
-#############################################################################################
-sudo cp Apt/{testing,unstable,experimental,security}.pref /etc/apt/preferences.d 					### THE SAME
 
 ####################
 # Begin Automation #
@@ -91,10 +76,10 @@ read -rn1 ans
 if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
     sudo apt remove --purge -y gnome. kde. xfce. cinnamon. mate. 
     clear
-    echo -e "DEs removed...\n"
+    echo -e "DEs removed!\n"
     sleep 2
 else
-    echo -e "\nNot removing DEs...\n"
+    echo -e "\nNot removing DEs!\n"
     sleep 2
 fi
 
@@ -107,7 +92,7 @@ sleep 1
 sudo apt install -y -m apt-transport-https # install the package for http redirect 
 sudo apt install -y --force-yes deb-multimedia-keyring # install the keyring for a repo
 sudo apt update
-sudo apt install -y -m firmware-linux-free tar unrar zip gzip git aptitude build-essential sudo wget ntp htop gksu # install the basic utilities
+sudo apt install -y -m firmware-linux-free tar unrar zip gzip git aptitude build-essential sudo wget ntp htop gksu e2fsprogs xfsprogs reiserfsprogs reiser4progs jfsutils ntfs-3g fuse gvfs gvfs-fuse fusesmb # install the basic utilities
 
 clear
 
@@ -115,21 +100,25 @@ echo "Rechecking Debian database..."
 sudo apt -y --force-yes dist-upgrade
 
 ###########################
-# more essential packages #
+# NETWORK #
+###########################
+clear
+echo "Network packages..."
+sleep 1
+sudo apt install wireless-tools firmware-linux firmware-iwlwifi firmware-ralink firmware-ipw2x00 firmware-realtek intel-microcode amd64-microcode network-manager-gnome telnet ssh
+
+
+###########################
+# SOUND #
 ###########################
 sleep 1
-sudo apt install -y alsa-base alsa-utils alsa-tools-gui alsamixergui aptitude network-manager-gnome pulseaudio pavumeter pavucontrol paprefs paman telnet ssh
+sudo apt install -y alsa-base alsa-utils alsa-tools-gui alsamixergui aptitude pulseaudio pavumeter pavucontrol paprefs paman
 
 ##############################################################
 # install LXDE #
 ##############################################################
 clear
 echo "Do you want to install LXDE? [N/y]"
-read -rn1 ans
-
-###############################################
-# Create the needed directories a bit earlier #
-###############################################
 if [ ! -d ~/docs ]; then
     mkdir ~/docs
 fi
@@ -138,14 +127,15 @@ if [ ! -d ~/docs/misc ]; then
     mkdir ~/docs/misc
     mkdir ~/docs/tools
 fi
+read -rn1 ans
 
     if [ "${ans:0:1}" = "N" ] || [ "${ans:0:1}" = "n" ]; then
 	echo "Not installing LXDE..."
     else
-	sudo apt install -m -y --no-install-recommends lxde-core lxde lxde-common task-lxde-desktop  lxde-settings-daemon lxde-icon-theme lightdm
+	sudo apt install -m -y --no-install-recommends lxde-core lxde lxde-common task-lxde-desktop lxde-settings-daemon lxde-icon-theme lightdm
 	sudo apt remove --purge -y wicd.
 	sudo apt install -y -m alsamixergui evince-gtk evolution gpicview lxpolkit menu-xdg lxsession lxtask lxterminal lxpanel lxappearance pcmanfm usermode xserver-xorg xscreensaver network-manager
-	sudo apt install -y --no-install-recommends xarchiver
+	sudo apt install -y --no-install-recommends xarchiver flashplugin-nonfree gmrun xinput suckless-tools xfce4-power-manager xfce4-power-manager-plugins
 
 	if [ ! -d ~/.config/lxpanel/LXDE/panels ]; then
 	    mkdir -p ~/.config/lxpanel/LXDE/panels
@@ -157,36 +147,41 @@ fi
 	if [ -d ~/.config/openbox ]; then
 	    mkdir -p ~/.config/openbox/
 	fi
+
+	if [ -d ~/pic ]; then
+	    mkdir -p ~/pic
+	fi
 	
-	#echo "wallpaper=$HOME/Pictures/Wallpapers/PDP-11.jpg" >> /tmp/config/LXDE/desktop-items-0.conf				### WOT??? CHECK IT
+	sudo cp wallpaper.jpg $HOME/pic/wallpaper.jpg
+	echo "wallpaper=$HOME/pic/wallpaper.jpg" >> /tmp/config/LXDE/desktop-items-0.conf				
 
 	if [ ! -f ~/.config/lxterminal/lxterminal ]; then
-	    cp Stuff/LXDE/lxterminal.conf ~/.config/lxterminal
+	    cp config/LXDE/lxterminal.conf ~/.config/lxterminal
 	fi
 	if [ ! -f ~/.config/lxsession/LXDE/autostart ]; then
-	    cp Stuff/LXDE/autostart ~/.config/lxsession/LXDE
+	    cp config/LXDE/autostart ~/.config/lxsession/LXDE
 	fi
 	if [ ! -f ~/.config/lxsession/LXDE/desktop.conf ]; then
-	    cp Stuff/LXDE/desktop.conf ~/.config/lxsession/LXDE
+	    cp config/LXDE/desktop.conf ~/.config/lxsession/LXDE
 	fi
 	if [ ! -f ~/.config/lxpanel/launchtaskbar.cfg ]; then
-	    cp Stuff/LXDE/launchtaskbar.cfg ~/.config/lxpanel
+	    cp config/LXDE/launchtaskbar.cfg ~/.config/lxpanel
 	fi
 	if [ ! -f ~/.config/lxpanel/LXDE/panels/panel ]; then
-	    cp Stuff/LXDE/panel ~/.config/lxpanel/LXDE/panels
+	    cp config/LXDE/panel ~/.config/lxpanel/LXDE/panels
 	fi
 	if [ ! -f ~/.config/pcmanfm/LXDE/desktop-items-0.conf ]; then
-	    cp Stuff/LXDE/desktop-items-0.conf ~/.config/pcmanfm/LXDE
+	    cp config/LXDE/desktop-items-0.conf ~/.config/pcmanfm/LXDE
 	fi
 	if [ ! -f ~/.config/openbox/lxde-rc.xml ]; then
-	    cp Stuff/LXDE/lxde-rc.xml ~/.config/openbox
+	    cp config/LXDE/lxde-rc.xml ~/.config/openbox
 	fi
     fi
 
 ################################################################################
 # some moved appearance related programs which I want installed no matter what #
 ################################################################################
-sudo apt install -m -y gtk-chtheme gtk-smooth-themes gtk-theme-config gtk-theme-switch gtk2-engines gtk2-engines-aurora gtk2-engines-cleanice gtk2-engines-magicchicken gtk2-engines-moblin gtk2-engines-murrine gtk2-engines-nodoka gtk2-engines-oxygen gtk2-engines-pixbuf gtk2-engines-qtcurve gtk2-engines-wonderland clearlooks-phenix-theme hunspell-en-us hyphen-en-us fonts-inconsolata
+sudo apt install -m -y gtk-chtheme gtk-smooth-themes gtk-theme-config gtk-theme-switch gtk2-engines gtk2-engines-aurora gtk2-engines-cleanice gtk2-engines-magicchicken gtk2-engines-moblin gtk2-engines-murrine gtk2-engines-nodoka gtk2-engines-oxygen gtk2-engines-pixbuf gtk2-engines-qtcurve gtk2-engines-wonderland clearlooks-phenix-theme hunspell-en-us hyphen-en-us fonts-inconsolata fonts-dejavu fonts-droid fonts-freefont-ttf fonts-liberation ttf-mscorefonts-installer
 
 ####################################
 # Add some good, everyday programs #
@@ -194,7 +189,8 @@ sudo apt install -m -y gtk-chtheme gtk-smooth-themes gtk-theme-config gtk-theme-
 clear
 echo "Now installing basic programs..."
 sleep 1
-sudo apt install -y --no-install-recommends aspell aspell-en openjdk-7-jdk openjdk-7-jre icedtea-plugin leafpad libreoffice libreoffice-gtk nano screenfetch shutter transmission-gtk synaptic gimp mg feh gdebi zsh mutt mpd ncmpcpp deadbeef yelp offlineimap
+sudo apt install -y --no-install-recommends aspell aspell-en openjdk-7-jdk openjdk-7-jre icedtea-plugin leafpad libreoffice libreoffice-gtk nano screenfetch shutter transmission-gtk synaptic gimp mg feh gdebi zsh mpd ncmpcpp yelp offlineimap galculator 
+sudo apt install -y -m bash-completion lintian libnss-mdns gvfs-bin gvfs-backends python-keybinder xdg-utils rsync anacron usbutils wmctrl menu bc screen cowsay figlet whois rpl cpufrequtils debconf-utils apt-xapian-index build-essential user-setup avahi-utils avahi-daemon ftp openssh-client sshfs
 
 #############################
 # Move a prompt bit earlier #
@@ -212,7 +208,7 @@ read -rn1 ans
 
 if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
     sudo apt install -y zsh
-    sudo cp /tmp/Stuff/General/zshrc ~/.zshrc
+    sudo cp /tmp/config/General/zshrc ~/.zshrc
     sudo chsh -s /bin/zsh $USER
     
     #echo "# do not prompt" > ~/.zshrc
@@ -247,7 +243,7 @@ if [ ! -d ~/docs/misc ]; then
     mkdir ~/docs/misc
 fi
 
-sudo apt install -y wipe keepass2 bleachbit gnupg evolution vlc lame sox vorbis-tools shutter 
+sudo apt install -y wipe bleachbit gnupg evolution vlc lame sox vorbis-tools shutter 
 
 #############
 # wgetpaste #
@@ -284,9 +280,6 @@ read -rn1 ans
 
 if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
     sudo apt install -y gtk-redshift redshift
-
-    cd /tmp
-    cp docs/Redshift ~/docs/misc/Redshift
     
     echo "Redshift has been installed."
 else
@@ -302,24 +295,12 @@ echo "Would you like to install another text editor or IDE? [Y/n]"
 read -rn1 ans
 
 if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
-    echo -e "\nWould you like to install Emacs? [Y/n]"
-    read -rn1 ans
-
-    if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
-	sudo apt install -y emacs
-	echo -e "VISUAL='emacs -nw'\nEDITOR='emacs'" >> ~/.bashrc
-	
-	cp /tmp/Stuff/General/emacs ~/.emacs
-	cp --recursive /tmp/Stuff/General/emacs.d ~/.emacs.d
-	cp /tmp/docs/Emacs ~/docs
-    fi
     
     echo -e "\nWould you like to install Geany? [Y/n]"
     read -rn1 ans
 	
     if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
 	sudo apt install -y geany geany-plugins
-	# insert configuration files here
     fi
     
     echo -e "\nWould you like to install vim? [Y/n]"
@@ -328,15 +309,28 @@ if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
     if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
 	sudo apt install -y vim vim-gnome
 	
-	# configuration files here plox
     fi
     
-    #echo -e "\nWould you like to install Atom? [Y/n]"
-    #read -rn1 ans
-    #
-    #if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
-	# instructions here
-    #fi
+    echo -e "\nWould you like to install Atom? [Y/n]"
+    read -rn1 ans
+    
+    if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
+	cd /tmp   
+	rm -f atom*
+	wget "https://github.com/atom/atom/releases/download/v1.2.4/atom-amd64.deb"
+	sudo dpkg --install atom-amd64.deb
+    fi
+	
+    echo -e "\nWould you like to install Sublime Text? [Y/n]"
+    read -rn1 ans
+    
+    if [ "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
+	cd /tmp   
+	rm -f sublime*
+	wget "http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_amd64.deb"
+	sudo dpkg -i sublime-text_build-3083_amd64.deb
+    fi
+
 fi
 	
 ###########
@@ -348,15 +342,17 @@ read -rn1 ans
 
 if [ ! "${ans:0:1}" = "Y" ] || [ "${ans:0:1}" = "y" ]; then
     sudo apt install -y -m compton compton-conf
-    echo "Compton has been installed."
+    echo "Compton installed!"
 else
-    echo "Compton will not be installed."
+    echo "Compton not installed!"
 
 fi
 
 #############
 # arc theme #
 #############
+clear
+echo -e "\nInstalling arc theme..."
 sudo apt -y install autoconf automake pkg-config libgtk-3-dev git
 
 if [ ! -d arc-theme ]; then
@@ -372,9 +368,9 @@ sudo make install
 ###############
 # numix stuff #									### CHECK THIS
 ###############
+echo -e "\nInstalling numix..."
 cd /tmp
-#sudo cp apt/numix.list /etc/apt/sources.list.d/			### add lines to sources.list
-sudo apt-key add Stuff/General/numix.key
+sudo apt-key add config/General/numix.key
 sudo apt update
 sudo apt install -y -m numix-{gtk,icon}-theme numix-wallpaper-{notd,saucy,fs,halloween,winter-chill,lightbulb,simple-things,mr-numix,kitty,mesh,aurora} numix-icon-theme-circle
 
@@ -382,42 +378,14 @@ sudo apt install -y -m numix-{gtk,icon}-theme numix-wallpaper-{notd,saucy,fs,hal
 # Chromium #
 #############
 
+clear
+echo -e "\nInstalling chromium..."
 sudo apt install -y chromium
-
-#######################
-# Documentation files #
-#######################
-cd /tmp
-cp docs/Bleachbit ~/docs/misc/Bleachbit
-cp docs/GPG ~/docs/misc/GPG
-cp docs/Keepass2 ~/docs/misc/Keepass2
-cp docs/Post-install ~/docs/Post-install
-cp docs/Shutter ~/docs/misc/Shutter
-cp docs/Ice ~/docs/misc/Ice
-cp docs/Startup ~/docs/startup
-cp docs/EXE ~/docs/EXE
-cp docs/Packagelist ~/docs/
-
-# Copy ALL the description files for reference.
-if [ ! -d /usr/share/doc/script ]; then
-    sudo mkdir /usr/share/doc/script
-fi
-
-sudo cp /tmp/docs/* /usr/share/doc/script
 
 #######################
 # Configuration files #
 #######################
 cp config/General/Xresources ~/.Xresources
-
-######################
-# Desktop background #								### CHECK THIS
-######################
-if [ ! -d ~/Pictures/Wallpapers ]; then
-    mkdir -p ~/Pictures/Wallpapers
-fi
-# add some nice, wallpaper selection
-wget https://imgrush.com/mCnbtcV_CedN.png -O "$HOME/Pictures/Wallpapers/Arch.png"
 
 ###############
 # Final steps #
@@ -430,16 +398,13 @@ sudo apt-get autoclean
 sudo chown -R $USER:$USER $HOME/* 
 sudo dpkg-reconfigure ntp
 sudo alsactl store
-sudo update-menus
-update-menus
 
-#####################################################################
-# Keep this in please!                                              #
-# Sometimes when LightDM is installed, that file becomes corrupted. #
-#####################################################################
-if [ -f ~/.Xauthority ]; then
+if [ -f ~/.Xauthority ]; then				## sometimes becomes corrupted after installing lightdm
     mv ~/.Xauthority ~/.Xauthority.old
 fi
+
+sudo update-menus
+update-menus
 
 #######
 # End #
