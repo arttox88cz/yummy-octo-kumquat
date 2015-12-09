203eb8f3172b28df0fcfe3d6b62503f7967db8f1
arttox88cz <jirka.maresh@gmail.com>
2015-12-09 22:15:43 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,32 @@
 #!/bin/bash
 # This runs the main script and cleans up when it fails.
-./main.sh; tput cnorm
+sudo apt install -y --force-yes deb-multimedia-keyring 
+sudo apt install -y -m tar unrar zip gzip git aptitude build-essential sudo wget ntp htop gksu e2fsprogs xfsprogs reiserfsprogs reiser4progs jfsutils ntfs-3g fuse gvfs gvfs-fuse fusesmb flashplugin-nonfree suckless-tools
+sudo apt install -m -y gtk-chtheme gtk-smooth-themes gtk-theme-config gtk-theme-switch gtk2-engines gtk2-engines-aurora gtk2-engines-cleanice gtk2-engines-magicchicken gtk2-engines-moblin gtk2-engines-murrine gtk2-engines-nodoka gtk2-engines-oxygen gtk2-engines-pixbuf gtk2-engines-qtcurve gtk2-engines-wonderland clearlooks-phenix-theme hunspell-en-us hyphen-en-us fonts-inconsolata fonts-dejavu fonts-droid fonts-freefont-ttf fonts-liberation ttf-mscorefonts-installer
+sudo apt install -y urxvt
+pcmanfm
+xarchiver
+palemoon
+hexchat
+pinta
+gpicview
+qpdfview
+clipit
+mousepad
+vim
+vlc
+lxappearance
+nitrogen
+compton
+arandr
+htop
+tree
+gparted
+blurlock
+alsa
+alsamixer
+volumeicon
+network-manager-gnome
+xfce-power-manager
+sudo apt install -y --no-install-recommends openjdk-7-jdk openjdk-7-jre icedtea-plugin libreoffice libreoffice-gtk transmission-gtk synaptic gimp mg gdebi zsh mpd ncmpcpp yelp offlineimap galculator 
+sudo apt install -y -m bash-completion lintian libnss-mdns gvfs-bin gvfs-backends python-keybinder xdg-utils rsync anacron usbutils wmctrl menu bc screen cowsay figlet whois rpl cpufrequtils debconf-utils apt-xapian-index build-essential user-setup avahi-utils avahi-daemon ftp openssh-client sshfs dconf-editor
